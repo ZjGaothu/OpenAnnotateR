c8c9b5ba7b249f0f0ceaa56af1d135f4b727e6ca
Gaozj <48663858+ZjGaothu@users.noreply.github.com>
2023-12-22 00:19:24 +0800

    Update Openness2Seurat.R

--- R/Openness2Seurat.R
+++ R/Openness2Seurat.R
@@ -4,10 +4,12 @@
 #' @import Seurat
 #' @param file_path : Path of download file.
 #' @export Openness2Seurat
-Openness2Seurat <- function(file_path)
+Openness2Seurat <- function(file_path,header_path)
 {
   open.mat <- read.table(file_path,header = FALSE)
+  header.mat <- read.table(header_path,header = FALSE)
   open.mat <- open.mat[,5:dim(open.mat)[2]]
-  pbmc <-CreateSeuratObject(counts = open.mat)
-  return(pbmc)
+  colnames(open.mat) <- header.mat[,1]
+  obj <-CreateSeuratObject(counts = open.mat)
+  return(obj)
 }
